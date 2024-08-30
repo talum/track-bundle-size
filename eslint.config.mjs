55f7d324811494fbfeaf5a11b7d81f09e2cecea9
Tracy Lum <11878752+talum@users.noreply.github.com>
2024-08-30 09:32:09 -0400

    Fix eslint, swap node-fetch for axios

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import prettierConfig from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{mjs,ts}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  prettierConfig,
 ];
